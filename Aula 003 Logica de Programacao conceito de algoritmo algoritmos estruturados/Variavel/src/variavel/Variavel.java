741608dc9a8cfa07bd50a28127e5f9b64b6a176c
azlinwei <azlinwei@gmail.com>
2021-08-03 15:49:48 -0300

    atualiza 2

--- Aula 003 Logica de Programacao conceito de algoritmo algoritmos estruturados/Variavel/src/variavel/Variavel.java
+++ Aula 003 Logica de Programacao conceito de algoritmo algoritmos estruturados/Variavel/src/variavel/Variavel.java
@@ -16,16 +16,19 @@ public class Variavel {
     public static void main(String[] args) {
         String str1 = "Ola mundo   ";
         String str2 = "Ola mundo \n";
-        int a = 6;
+        int a = 16;
         int b = 7;
         
         
         System.out.print(str1);
         System.out.println(str2);
         System.out.print(str2 + str1);
-        System.out.printf("Soma da variveis a e b = %d ", (a + b));
-        
-        
+        System.out.printf("Soma das variveis a e b = %d \n", (a + b));
+        System.out.printf("Subtracao das variveis a e b = %d \n", (a - b));
+        System.out.printf("Multiplicacao das variveis a e b = %d \n", (a * b));
+        System.out.printf("Divisao das variveis a e b = %d \n", (a / b));
+        System.out.printf("Divisao das variveis a e b = %d \n", (a % b));
+     
     }
     
 }
