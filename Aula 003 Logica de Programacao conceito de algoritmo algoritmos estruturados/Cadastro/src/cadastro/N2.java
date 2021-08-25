38d140a4771031d4bed2cd3b60f36903ec5e9074
azlinwei <azlinwei@gmail.com>
2021-08-25 14:38:48 -0300

    Adicionando projeto

---
+++ Aula 003 Logica de Programacao conceito de algoritmo algoritmos estruturados/Cadastro/src/cadastro/N2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastro;
+
+/**
+ *
+ * @author azlin
+ */
+public class N2 extends N1{
+    public int numS = 12345789;
+    public int valor = 500;
+}
---
+++ Aula 003 Logica de Programacao conceito de algoritmo algoritmos estruturados/Cadastro/src/cadastro/N3.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastro;
+
+/**
+ *
+ * @author azlin
+ */
+public class N3 extends N2{
+    public String nomeDeF = "Senac";
+}
